9ad84753f199a3048c58c05094aaf04eb378d064
Nicolas MERCIER <nicolas.mercier1999@gmail.com>
2020-03-11 14:42:30 +0100

    Creates the home controller Install the security-checker

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,17 @@
 
 	use Symfony\Component\Routing\Annotation\Route;
 	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-	class HomeController
+	class HomeController extends AbstractController
 	{
 		/**
 		 *	@Route("/", name="homepage")
 		 */
-		public function homepage()
+		public function homepage($slug)
 		{
-			return new Response('Page RecipeController');
+			return $this->render('index.html.twig', [
+				'title' => ucwords(str_replace('-', ' ', $slug))
+			])
 		}
 	}
